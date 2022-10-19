b5fc2ebb38c830b619e736c83849df31d1d555b6
AzAbid1 <73388494+AzAbid1@users.noreply.github.com>
2022-10-19 16:54:40 +0100

    add transaction

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  final Function addTx;
+
+  NewTransaction(this.addTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+            ),
+            TextButton(
+              onPressed: () {
+                addTx(titleController.text, double.parse(amountController.text));
+              },
+              child: Text('Add Transaction'),
+              style: TextButton.styleFrom(foregroundColor: Colors.purple),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
